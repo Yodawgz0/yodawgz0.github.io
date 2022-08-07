2b244c31c8dd02862ae039d6a2aa821f49f5a959
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2022-08-07 22:53:16 +0530

    adding Skills Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "././Styles/App.scss";
 import { NavigationBar } from "./components/NavigationBar";
 import { HomePage } from "./components/HomePage";
-
+import { SkillsPage } from "./components/SkillsPage";
 function App() {
   return (
     <div className="App">
       <NavigationBar />
       <HomePage />
+      <SkillsPage />
     </div>
   );
 }
