20fa83b6a84f01fc704ac4384e81b814c91ad452
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2022-08-16 15:15:37 +0530

    slider  change

--- src/features/skillsSlider.js
+++ src/features/skillsSlider.js
@@ -1,16 +1,21 @@
 import Slider from "react-slick";
 import "../Styles/SkillsPageStyles.scss";
 import { ProgressBar } from "react-bootstrap";
+import {
+  CircularProgressbarWithChildren,
+  buildStyles,
+} from "react-circular-progressbar";
 
 export default function SkillsSlider() {
   const data = [
     { skill: "ReactJS", percent: 80 },
     { skill: "JavaScript", percent: 80 },
-    { skill: "React-Native", percent: 30 },
+    { skill: "React-Native", percent: 50 },
     { skill: "CSS3/SASS", percent: 75 },
     { skill: "TCP/UDP", percent: 50 },
     { skill: "Python", percent: 95 },
     { skill: "HTML5", percent: 80 },
+    { skill: "APIs", percent: 60 },
   ];
   const SliderSettings = {
     infinite: true,
@@ -50,17 +55,30 @@ export default function SkillsSlider() {
   return (
     <div className="skillSlider">
       <Slider {...SliderSettings}>
-        {data.map((Element, i) => {
+        {data.map((element, i) => {
           return (
             <div className="skillSliderBarMain">
-              <span className="skillSliderBarText">{Element.skill}</span>
-              <ProgressBar
-                className="skillSliderBar"
-                key={i}
-                animated
-                variant="info"
-                now={Element.percent}
-              />
+              <CircularProgressbarWithChildren
+                className="progressBar"
+                styles={buildStyles({
+                  trailColor: "#000000",
+                  pathColor: "#01FFFF",
+
+                  strokeLinecap: "butt",
+                })}
+                strokeWidth={9}
+                value={element.percent}
+              >
+                <div
+                  style={{
+                    fontSize: "2.2ch",
+                    marginTop: -5,
+                    color: "#fff",
+                  }}
+                >
+                  {element.skill}
+                </div>
+              </CircularProgressbarWithChildren>
             </div>
           );
         })}
