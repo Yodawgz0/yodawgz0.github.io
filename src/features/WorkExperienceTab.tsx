87ec8c20808be718c84540e70986ee84eae3fd57
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-09-19 17:32:27 -0400

    updating-work-ex

--- src/features/WorkExperienceTab.tsx
+++ src/features/WorkExperienceTab.tsx
@@ -28,19 +28,25 @@ const WorkExperienceTab = () => {
       <div>
         <ul>
           <li>
-            Responsibility full stack web applications using React, Node.js,
-            Express.js, and MongoDB, increasing UI performance by 40% and user
-            satisfaction by 25%.
+            Implemented React, Node.js, Express.js, and MongoDB to build
+            high-performing full-stack web applications, driving a significant
+            40\% enhancement in UI performance and elevating user satisfaction
+            by 25\%
           </li>
           <li>
-            Enhanced code quality and security by migrating to TypeScript and
-            integrating JWT authentication; improved API performance by
-            optimizing NoSQL queries, reducing response time by 80%.
+            Led a seamless transition from JavaScript to TypeScript, bolstering
+            code quality and security; integrated JWT authentication for
+            enhanced user data protection, leading to an impressive 80\%
+            decrease in API response time
           </li>
           <li>
-            Applied test-driven development (TDD) using Cypress and Jest,
-            ensuring code reliability and coverage. Automated additional 10-15%
-            test cases, saving time and resources
+            Optimized code reliability and coverage through the implementation
+            of test-driven development (TDD) methodology using Cypress and Jest
+            frameworks. Increased automated test cases by 10-15\%
+          </li>
+          <li>
+            Orchestrated the automation of 75\% of workflows through Python
+            scripting
           </li>
         </ul>
       </div>
@@ -57,20 +63,27 @@ const WorkExperienceTab = () => {
       <div>
         <ul>
           <li>
-            Created front-end of a test web application, enabling users to test
-            web components, APIs, performance, and core web vitals. Reduced
-            manual testing workload by 50%. Server as of nodejs.
+            Spearheaded the development and execution of a groundbreaking test
+            web application, leveraging Node.js, to enable comprehensive
+            evaluations of web components, APIs, performance, and core web
+            vitals. Achieved a remarkable 50\% reduction in manual testing
+            efforts
           </li>
           <li>
-            • Utilized JavaScript, ReactJS with hooks, JSON, and Webpack,
-            integrating Redux and Thunk for state management; Gulp for task
-            execution, decreasing deployment time by 20%
+            Revamped website user interface using JavaScript, ReactJS with
+            hooks, and JSON; integrated Redux and Thunk for efficient state
+            management, resulting in a 20\% decrease in deployment time
           </li>
           <li>
-            Employed Webpack, ESLint, and Gulp for bundling and linting
-            workflows, reducing client load by 30%. Leveraged libraries such as
-            React Bootstrap, Material UI, and Chart.js for UI design and data
-            visualization.
+            Implemented React Bootstrap, Ant-D, and Material UI libraries to
+            craft visually captivating data visualizations, resulting in a 25\%
+            increase in user engagement and a 15\% decrease in bounce rate.
+          </li>
+          <li>
+            Streamlined collaboration across four cross-functional teams by
+            implementing JIRA, Confluence, GitLab, and Bitbucket; achieved a
+            remarkable 40\% reduction in project delivery time and boosted
+            stakeholder satisfaction by 20\%
           </li>
         </ul>
       </div>
@@ -115,8 +128,8 @@ const WorkExperienceTab = () => {
         <br />
         <span className="subFontItalicsTitle">Developer Tools: </span>
         <span className="subFontItalicsSKills">
-          GitHub, Linux, VS-Code, PyCharm, React Dev, Chome/Edge/Moz Dev,
-          PostMan, Compass
+          GitHub, Figma, Chome/Edge/Moz Dev, gulp, ESLint, Android Studio,
+          PostMan, Redis
         </span>
         , gulp, ESLint, webpack, Android Studio <br />
         <span className="subFontItalicsTitle">Libraries: </span>
